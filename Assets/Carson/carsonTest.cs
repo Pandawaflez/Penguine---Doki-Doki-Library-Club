6a9e340b26c3eba74ec51da492e5d7855593e5cc
Pandawaflez <179869288+Pandawaflez@users.noreply.github.com>
2024-09-21 15:20:11 -0700

    Carson Test Function added

---
+++ Assets/Carson/carsonTest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carsonTest : MonoBehaviour
+{
+    
+    public void carsonTestFunc () {
+        Debug.Log("Ballsagna");
+    }
+    
+    // Start is called before the first frame update
+    void Start() {
+        carsonTestFunc();
+    }
+    
+}
