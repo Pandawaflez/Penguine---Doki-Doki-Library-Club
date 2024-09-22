2ffec433bafe59ba35e3d28ce1bf77cf0558a051
Lance <85811392+Lance-Town@users.noreply.github.com>
2024-09-21 21:31:58 -0700

    player paddle moves correctly
    - fixed dynamic binding issue causing player paddle to not move correctly

--- Assets/Lance/Paddle.cs
+++ Assets/Lance/Paddle.cs
@@ -6,10 +6,15 @@ public class Paddle : MonoBehaviour
 {
     protected float speed = 10;
     protected float height;
+    protected static Vector2 bottomLeft;
+    protected static Vector2 topRight;
     // Start is called before the first frame update
     void Start()
     {
         height = transform.localScale.y;
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0,0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        Debug.Log("Height: " + height);
     }
 
     // Update is called once per frame
--- Assets/Lance/PlayerPaddle.cs
+++ Assets/Lance/PlayerPaddle.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class PlayerPaddle : Paddle
 {
     string input = "PlayerPaddle";
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update()
-    {
+    private void Update() {
+        // GetAxis is a number between -1 to 1 (-1 for down, 1 for up)
         float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        // restrict paddle movement so it doesn't go offscreen
+        if (transform.position.y < bottomLeft.y + height / 2 && move < 0) {
+            move = 0;
+        } else if (transform.position.y > topRight.y - height / 2 && move > 0) {
+            move = 0;
+        }
+
+//        Debug.Log("Move Value: " + move + "\tMovement Speed: " + speed);
+
+        transform.Translate(move * Vector2.up);
     }
 }
