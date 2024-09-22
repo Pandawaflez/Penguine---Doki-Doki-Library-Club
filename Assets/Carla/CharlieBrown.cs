f821ce0edd1b82667776cb150c49ad8825238a3c
2trainzz <crkolze@gmail.com>
2024-09-22 16:34:39 -0700

    initiateMiniGame() calls pong

--- Assets/Carla/CharlieBrown.cs
+++ Assets/Carla/CharlieBrown.cs
@@ -5,6 +5,7 @@ using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CharlieBrown : Peanuts
 {
@@ -12,7 +13,7 @@ public class CharlieBrown : Peanuts
     //sfx
     //minigame
 
-    public TextMeshProUGUI dialogueText, response1Text, response2Text, justText;
+    public TextMeshProUGUI dialogueText, response1Text, response2Text;
 
     /*dialogue order: 
     0->a1|b2
@@ -29,29 +30,29 @@ public class CharlieBrown : Peanuts
     private string d0response1 = "Live a little Charlie! What harm will come from speaking loudly?";
     private string d0response2 = "Oh, of course! So nice to meet you Charlie";
 
-    public string dialogue1 = "Don't you know the librarians can lock you up in the Ancient book room?! I can't associate with you anymore.";
-    public string d1response1 = "Don't be such a scaredy cat Charlie Brown";
-    public string d1response2 = "Good Grief! Good thing you warned me!";
+    private string dialogue1 = "Don't you know the librarians can lock you up in the Ancient book room?! I can't associate with you anymore.";
+    private string d1response1 = "Don't be such a scaredy cat Charlie Brown";
+    private string d1response2 = "Good Grief! Good thing you warned me!";
 
-    public string dialogue2 = "So what brings you to my corner of the library?";
-    public string d2response1 = "I'm hunting rats";
-    public string d2response2 = "I'm just looking for something to do while I wait for my little sister to pick out her comics.";
+    private string dialogue2 = "So what brings you to my corner of the library?";
+    private string d2response1 = "I'm hunting rats";
+    private string d2response2 = "I'm just looking for something to do while I wait for my little sister to pick out her comics.";
 
-    public string dialogue3 = "Good Grief! Are there really rats in here?";
-    public string d3response1 = "No smh that was clearly a joke";
-    public string d3response2 = "Yeah but don't worry pookie, I'll keep you safe";
+    private string dialogue3 = "Good Grief! Are there really rats in here?";
+    private string d3response1 = "No smh that was clearly a joke";
+    private string d3response2 = "Yeah but don't worry pookie, I'll keep you safe";
 
-    public string dialogue4 = "Oh what a coincidence! I have a little sister roaming around too. Do you want to play a game with me while we wait?";
-    public string d4response1 = "No I just want to read a book";
-    public string d4response2 = "Holy Smokes yeah";
+    private string dialogue4 = "Oh what a coincidence! I have a little sister roaming around too. Do you want to play a game with me while we wait?";
+    private string d4response1 = "No I just want to read a book";
+    private string d4response2 = "Holy Smokes yeah";
 
-    public string dialogue5 = "I'm just going to go back to reading then...";
+    private string dialogue5 = "I'm just going to go back to reading then...";
 
-    public string dialogue6 = "Oh umm. Thank you. Well since you're here, want to play a game?";
-    public string d6response1 = "Unfortch there's no rest for the wicked. See ya!";
-    public string d6response2 = "Yes, my sweet prince!";
+    private string dialogue6 = "Oh umm. Thank you. Well since you're here, want to play a game?";
+    private string d6response1 = "Unfortch there's no rest for the wicked. See ya!";
+    private string d6response2 = "Yes, my sweet prince!";
 
-    public string dialogue7 = "I'm reading, sorry.";
+    private string dialogue7 = "I'm reading, sorry.";
 
     public string dialogue8 = "Thanks for playing with me!";
 
@@ -64,13 +65,14 @@ public class CharlieBrown : Peanuts
         //call sfx. should only play 3 sec
     }
 
+    //game objects to set buttons active/deactive
     public GameObject r1p;
     public GameObject r2p;
     public void displayJustText(string dialogue){
         dialogueText.SetText(dialogue);
         r2p.SetActive(false);
         r1p.SetActive(false);
-        //r1.gameObject.SetActive(false);
+        //r1.gameObject.SetActive(false); //these didn't work for no reason smh
         //r2.gameObject.SetActive(false);
     }
 
@@ -86,10 +88,12 @@ public class CharlieBrown : Peanuts
         Time.timeScale = 1;
     }
 
+    //buttons to reset the buttons ('unclick' them)
     public Button r1;
     public Button r2;
     public int responseNum = 0;
 
+    //button one calls onclick
     public void hitResponse1(){
         responseNum = 1;
         Debug.Log("they hit it boss");
@@ -98,6 +102,8 @@ public class CharlieBrown : Peanuts
         r1.Select();
         toNextDialogue();
     }
+
+    //button 2 calls onclick
     public void hitResponse2(){
         responseNum = 2;
         //holdUp(1);
@@ -105,17 +111,15 @@ public class CharlieBrown : Peanuts
         toNextDialogue();
     }
 
-    public void initiateMiniGame(){}
+    public void initiateMiniGame(){
+        SceneManager.LoadScene("Pong");
+    }
 
+    //a button being hit will trigger this. decides how to respond to response
     public void toNextDialogue(){
         int d = getDialogueNum();
         switch(d){
             case 0:
-            Debug.Log("first time here huh");
-                //displayDialogue(dialogue0, d0response1, d0response2);
-                //displayJustText("umm");
-                //play sfx here or in above?
-                //if r1, dialogueNum=1, else 2
                 if (responseNum == 1){
                     dialogueNum=1;
                     updateAffection(-1);
@@ -124,13 +128,8 @@ public class CharlieBrown : Peanuts
                     updateAffection(5);
                 }
                 responseNum = 0;
-                //onDialogue(dialogueNum);
-                
                 break;
             case 1:
-            Debug.Log("here i stand");
-                //displayDialogue(dialogue1, d1response1, d1response2);
-                //if r1, dialogueNum=5, else 2
                 if (responseNum == 1){
                     dialogueNum = 5;
                     updateAffection(-2);
@@ -142,7 +141,6 @@ public class CharlieBrown : Peanuts
 
                 break;
             case 2:
-                displayDialogue(dialogue2, d2response1, d2response2);
                 //if r1, dialogueNum=3, else 4
                 if (responseNum == 1){
                     dialogueNum = 3;
@@ -154,7 +152,6 @@ public class CharlieBrown : Peanuts
                 responseNum = 0;
                 break;
             case 3: 
-                displayDialogue(dialogue3, d3response1, d3response2);
                 //if r1, dialogueNum=5, else 6
                 if (responseNum == 1){
                     dialogueNum = 5;
@@ -166,8 +163,6 @@ public class CharlieBrown : Peanuts
                 responseNum = 0;
                 break;
             case 4:
-                displayDialogue(dialogue4, d4response1, d4response2);
-                //if r1, dialogueNum=5, else playMini()
                 if (responseNum == 1){
                     dialogueNum = 5;
                     updateAffection(0);
@@ -179,14 +174,11 @@ public class CharlieBrown : Peanuts
                 responseNum = 0;
                 break;
             case 5:
-                displayJustText(dialogue5);
                 //player stops talking to charlie...
                 //holdUp(5); //useless?
                 dialogueNum=7;
                 break;
             case 6:
-                displayDialogue(dialogue6, d6response1, d6response2);
-                //if r1, dialogueNum=5, else playMini()
                 if (responseNum == 1){
                     dialogueNum = 5;
                     updateAffection(-2);
@@ -198,20 +190,23 @@ public class CharlieBrown : Peanuts
                 responseNum = 0;
                 break;
             case 7:
-                displayJustText(dialogue7);
                 //stuck
                 break;
             case 8:
-                displayJustText(dialogue8);
+                //post game
+                //give user option to keep talking?
                 break;
         }
         onDialogue(dialogueNum);
         Debug.Log(string.Format("current affection points: {0}", getAffectionPoints()));
     }
 
+    //prints the new dialogue
     public void onDialogue(int d){
         switch(d){
             case 0:
+            r2p.SetActive(true);
+            r1p.SetActive(true);
             Debug.Log("first time here huh");
                 displayDialogue(dialogue0, d0response1, d0response2);
                 
@@ -262,7 +257,6 @@ public class CharlieBrown : Peanuts
         dialogueText.SetText(dialogue0);
         response1Text.SetText(d0response1);
         response2Text.SetText("my sweet sweet charlie prince...");
-        displayJustText("umm");
         */
 
     }
--- Assets/Carla/Peanuts.cs
+++ Assets/Carla/Peanuts.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
 public abstract class Peanuts : MonoBehaviour
 {
     //graphics
