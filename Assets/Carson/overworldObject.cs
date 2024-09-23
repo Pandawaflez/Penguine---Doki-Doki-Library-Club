5ab7d206c705daff2e4d6122e5b7cadeed37fcf1
Pandawaflez <179869288+Pandawaflez@users.noreply.github.com>
2024-09-22 20:39:57 -0700

    Overworld Debug Pannel Start

--- Assets/Carson/overworldObject.cs
+++ Assets/Carson/overworldObject.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class overworldObject : MonoBehaviour
 {
-    public 
     
     // Start is called before the first frame update
     void Start()
---
+++ Assets/overworldDebugMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class overworldDebugMenu : MonoBehaviour
+{
+    public void talkTo( string character ){
+        Debug.Log("Character:");
+        switch ( character ){
+            case "Charlie":
+                Debug.Log("Talk to Chuck");
+                break;
+            case "Lucy":
+                Debug.Log("Talk to Lucy");
+                break;
+            case "Schroeder":
+                Debug.Log("Talk to Schroeder (AKA Kevin)");
+                break;
+            case "Snoopy":
+                Debug.Log("Talk to Snoopy");
+                break;
+            case "Shaggy":
+                Debug.Log("Talk to Norville");
+                break;
+            case "Daphne":
+                Debug.Log("Talk to Daphne");
+                break;
+            case "Fred":
+                Debug.Log("Talk to Frederick");
+                break;
+            case "Sonic":
+                Debug.Log("Talk to Sanic");
+                break;
+            case "Shadow":
+                Debug.Log("Talk to Shadow");
+                break;
+            default:
+                Debug.Log("Character " + character + " not found");
+                break;
+        }
+    }
+}
