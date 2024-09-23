87c5bc36490792f581846a7715135f20dc4393bf
2trainzz <crkolze@gmail.com>
2024-09-22 20:15:35 -0700

    trying to make a static database

---
+++ Assets/Carla/PeanutsDB.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PeanutsDB
+{
+    public static int CharlieDialogue{get; set;}
+    public static int CharlieAffectionPts{get; set;}
+    
+   
+}
