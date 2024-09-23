8725070e29b8e6b957bc944cb9e66d8e577db44f
Pandawaflez <179869288+Pandawaflez@users.noreply.github.com>
2024-09-22 21:11:28 -0700

    Scene Manager 1.7

--- Assets/SceneChanger.cs
+++ Assets/SceneChanger.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public static class sceneChanger : MonoBehaviour
+public static class SceneChanger
 {
-    private string SavedScene;
+    private static string SavedScene;
     //save scene to load later (set Saved Scene)
     public static void saveScene (){
-        m_Scene = SceneManager.GetActiveScene();
+        Scene m_Scene = SceneManager.GetActiveScene();
         Debug.Log("Saving Scene " + m_Scene);
         SavedScene = m_Scene.name;
     }
     //load the scene that was just here:
-    public static string Continue () {
-        Debug.Log("Loading Scene " + m_Scene);
+    public static void Continue () {
+        Debug.Log("Loading Scene " + SavedScene);
         SceneManager.LoadScene(SavedScene);
     }
+    /*
     //return the saved scene, probably will be just used for testing shrug
     public string getSavedScene(){
         return SavedScene;
-    }
+    }*/
 }
