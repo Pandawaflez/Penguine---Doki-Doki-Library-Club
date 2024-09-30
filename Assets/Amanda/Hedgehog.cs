7b719d53679ef530b463ba5ec5d845a4ff75a955
AmandaBoard <boar9227@vandals.uidaho.edu>
2024-09-30 15:39:13 -0700

    Sonic Script Update

--- Assets/Amanda/Hedgehog.cs
+++ Assets/Amanda/Hedgehog.cs
@@ -1,39 +1,27 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Hedgehog : MonoBehaviour
+public abstract class Hedgehog : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //graphics
+    //sfx
+    //diaglogues
+    /* private string sonicD1;
+    private string sonicD2;
+    private string sonicD3;
+    private string sonicD4;
+    protected string sonicD5 = "more coming soon"; 
+    */
+    
+    //dialogue responses
+    //dialogue tracking
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    protected int HdialogueNum = 0;
+
+    //affection points
+    public int HAP;
+
+    
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Hedgehog : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
->>>>>>> 555864fb9c505fb3eead53b1cd64672aafcf3cce
