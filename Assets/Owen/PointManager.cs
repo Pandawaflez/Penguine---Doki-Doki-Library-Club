83cfb979ba17eafe457873d8c0a77f4e14a71903
Owen McDaniel <mcdanielowen11@gmail.com>
2024-09-30 20:40:55 -0700

    Added scripts for MVP

---
+++ Assets/Owen/PointManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private int charPoints;
+
+    void Start()
+    {
+        
+    }
+
+
+    // Make reference to characters, and int for how much to increase points
+    private void AddPoints()
+    {
+
+    }
+
+    // Make reference to characters, and int for how many points to check if the points are 
+    private bool CheckPoints()
+    {
+        Debug.Log("POINT MANAGER TEST FOR MVP - OWEN");
+        return true;
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
