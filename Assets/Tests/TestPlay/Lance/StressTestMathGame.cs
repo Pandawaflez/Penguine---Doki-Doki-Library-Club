9505cb901bfcd72303429a6924684c4fe74c333f
Lance <85811392+Lance-Town@users.noreply.github.com>
2024-10-16 18:16:47 -0700

    add math game stress test
    - Stress tests math game with loading 5000 prefabs

---
+++ Assets/Tests/TestPlay/Lance/StressTestMathGame.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class StressTestMathGame
+{
+    public GameObject mathPrefab;
+
+    [SetUp]
+    public void SetUp()
+    {
+        mathPrefab = Resources.Load<GameObject>("Lance/MathPrefab");
+        Assert.IsNotNull(mathPrefab, "Math prefab not assigned in the Resources folder.");
+    }
+
+    [UnityTest]
+    public IEnumerator TestMultipleMathGameInstances()
+    {
+        // Spawn multiple instances
+        int instanceCount = 5000;
+        for (int i = 0; i < instanceCount; i++)
+        {
+            // Instantiate the prefab
+            GameObject instance = Object.Instantiate(mathPrefab);
+            instance.name = "MathInstance_" + i; // Name the instances for debugging
+            instance.transform.position = new Vector3(i * 2, 0, 0); // Spread them out
+        }
+
+        yield return null; // Allow the game objects to initialize
+
+        // Assert that the correct number of instances are in the scene
+        Assert.AreEqual(instanceCount, GameObject.FindObjectsOfType<Math>().Length, "Not all math game instances were created successfully.");
+    }
+}
