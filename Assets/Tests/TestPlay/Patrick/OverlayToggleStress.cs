b7f5e777e917e3ff7376f273534a6c61a96cd014
Patrick Du <du1631@vandals.uidaho.edu>
2024-10-16 17:07:44 -0700

    Made Tests

---
+++ Assets/Tests/TestPlay/Patrick/OverlayToggleStress.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class OverlayToggleStress
+{
+    [UnityTest]
+    public IEnumerator StressTest_UICheckInputHeavyLoad()
+    {
+        //Arrange: Create the UIElementHandler singleton GameObject
+        var handlerObject = new GameObject("UIHandler");
+        var handler = handlerObject.AddComponent<UIElementHandler>();
+
+        int iterations = 100000;  //Number of input checks to simulate
+        int logCount = 0;       //Track how many times the overlay is toggled
+
+        /* Act: Simulate heavy calls to UICheckInput.
+        since we can't simulate key presses in Unity testing apparently,
+        simulating UICheckInput many times to test under load.*/
+        for (int i = 0; i < iterations; i++)
+        {
+            //Manually call UICheckInput() since we're testing performance
+            handler.UICheckInput();
+
+            //Simulate a log message that would be produced on toggle
+            Debug.Log("*****Overlay Toggle*****");
+            logCount++;
+        }
+
+        yield return null;
+
+        //Assert: Ensure the correct number of toggles were logged
+        Assert.AreEqual(iterations, logCount, 
+            $"Expected {iterations} logs, but got {logCount}.");
+    }
+}
