6bb2452ea0067e26a6391e39024de072d909dc6b
Lance <85811392+Lance-Town@users.noreply.github.com>
2024-10-23 20:12:38 -0700

    remove unused variables and functions

--- Assets/Lance/MiniGameLevel.cs
+++ Assets/Lance/MiniGameLevel.cs
@@ -7,9 +7,7 @@ public class MiniGameLevel : MonoBehaviour
 {
     protected bool isGameOver = false; // keep track of state of game
     protected int timeLimit = 60; // time limit in seconds for each mini game
-    protected bool isRegularMode = true; // default game to being played in regular mode instead of BC mode
     protected int affectionReward = 10; // default number of affection points given for mini game
-    protected string previousScene; // previous scene to return back to after mini game ends
 
     // update the affection points reward for mini game
     public void ChangeAffectionPointReward(int reward) {
@@ -21,20 +19,9 @@ public class MiniGameLevel : MonoBehaviour
         isGameOver = state;
     }
 
-    // set the difficulty mode for the level. True is regular mode, false is BC mode
-    public void SetDifficultyMode(bool difficulty) {
-        isRegularMode = difficulty;
-    }
-
     // check if the game is over or not
     public bool CheckWinCondition() {
         return isGameOver;
-    }
-
-    public void StartMiniGame(string prevScene) {
-        previousScene = prevScene;
-
-
     }
 
     // handle the continue button click at the end of the game
@@ -47,7 +34,5 @@ public class MiniGameLevel : MonoBehaviour
     public virtual void EndGame() {
         Debug.Log("Ending the Game");
         // Time.timeScale = 0f;
-        // more to come? Maybe, idk. Carson will tell me. He tells me all truths of the universe.
-        SceneManager.LoadScene(previousScene);
     }
 }
