5c619123dbdfd4a50fc8f9d53a49dc438e7c6aca
Pandawaflez <179869288+Pandawaflez@users.noreply.github.com>
2024-11-05 10:24:09 -0800

    Character images updated. Dynamic binding computer easteregg fully implimented (talk tuah)

--- Assets/Carson/computerRoom.cs
+++ Assets/Carson/computerRoom.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class ComputerRoom : Room
 {
+    private GameObject computer;
     public override void loadRoom(){
         Debug.Log("Loading Computer Lab Room: " + name );
+        computer.SetActive(true);
     }
+
+    public override void setComputer ( GameObject x ) {
+        computer = x;
+        Debug.Log("Computer set." );
+    }
+
 }
