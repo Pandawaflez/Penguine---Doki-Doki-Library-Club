dfed975d3ae0a01ba73c04cbf81ba6f7bb2279ff
2trainzz <crkolze@gmail.com>
2024-11-18 14:37:27 -0800

    bang done. mini game pair done. take that carson gustabel.

--- Assets/Lance/RPS/RPS.cs
+++ Assets/Lance/RPS/RPS.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 using UnityEngine;
 //using System.Random;
 
 
 public class RPS : MiniGameLevel
 {
-    private ScoreManager _scoreManager;
+    // private ScoreManager _scoreManager;
     [SerializeField] GameObject gameOverScreen;
+    [SerializeField] TextMeshProUGUI winnerText;
+    [SerializeField] GameObject rpsGameScreen;
 
     public int compMove; //0=rock, 1=paper, 2=scissors
     public int didWin = -1; // -1 means they haven't won or lost yet
@@ -17,6 +22,10 @@ public class RPS : MiniGameLevel
     void Start()
     {
         compMove = Random.Range(0,3);
+        //Debug.Log(compMove);
+        Debug.Log("play loser");
+        gameOverScreen.SetActive(false);
+        rpsGameScreen.SetActive(true);
     }
 
     // Update is called once per frame
@@ -30,9 +39,11 @@ public class RPS : MiniGameLevel
             Start();
         } else if (compMove == 1){
             didWin = 0; // they lost
+            winnerText.text = "You lost!";
             VEndGame();
         } else {
             didWin = 1; // they won
+            winnerText.text = "You won!";
             VEndGame();
         }
     }
@@ -68,15 +79,16 @@ public class RPS : MiniGameLevel
         if (didWin == -1) {
             // player has not won or lost yet
             return;
-        } else if (didWin == 0) {
-            // player lost - not epic
-            MainPlayer.SetMiniGameStatus(0);
-        } else if (didWin == 1){
+        } else if (didWin == 1 || MainPlayer.IsBCMode()) {
             MainPlayer.SetMiniGameStatus(1); // epic
+        } else if (didWin == 0){
+            // player lost - not epic
+            MainPlayer.SetMiniGameStatus(0); // not epic
         } else {
             Debug.Log("What happened?"); // this shouldn't happen
         }
 
+        rpsGameScreen.SetActive(false);
         gameOverScreen.SetActive(true); // continue screen
     }
 }
